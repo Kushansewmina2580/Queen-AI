9d3e669fcc6801fc11f258e29b28f38c549e8e8f
Kushansewmina2580 <112306462+Kushansewmina2580@users.noreply.github.com>
2023-04-30 07:46:05 +0530

    CHANGE KUSHAN S

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ const chalk = require('chalk')
 
 global.session = process.env.SESSION_ID || "PUT HERE"; //enter your session id here
 
-global.openaikey = process.env.OPEN_AI_KEY || "ENTER_API-KEY_HERE"; //Put Your Open AI Api key
+global.openaikey = process.env.OPEN_AI_KEY || "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"; //Put Your Open AI Api key
 
-global.ownername = process.env.OWNER_NAME || "DarkWinzo"; //Queen-AI Owner Name here
+global.ownername = process.env.OWNER_NAME || "KUSHAN SEWMINA"; //Queen-AI Owner Name here
 
-global.ownernumber = process.env.OWNER_NUMBER || "94775200935"; //Owner whatsapp number here
+global.ownernumber = process.env.OWNER_NUMBER || "94725668277"; //Owner whatsapp number here
 
 global.language = "EN"; //Enter bot working language EN = English / SI = Sinhala /ML = Malayalam
 
